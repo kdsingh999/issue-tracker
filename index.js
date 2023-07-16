1d19547d7dd8898c23409ec0d6bf6756f64f6884
KD Singh <rajputkdsingh999@gmail.com>
2023-07-16 18:03:02 +0530

    create all folders

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+require('dotenv').config({
+  path: './config/config.env',
+});
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.listen(5000, (err) => {
-  if (!err) console.log('Server is started');
+app.listen(PORT, (err) => {
+  if (!err) console.log(`Server is started at ${PORT}`);
   else console.log('Server is not started');
 });
